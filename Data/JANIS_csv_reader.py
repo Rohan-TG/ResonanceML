ea5287a47cc3a6433dacaeda4853c0316b783910
RNTG <rohan.tg05@gmail.com>
2024-04-11 16:50:21 +0100

    progress

--- Data/JANIS_csv_reader.py
+++ Data/JANIS_csv_reader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import periodictable
 
 df = pd.read_csv('table (1).csv',
 				 # header=2,
@@ -24,4 +25,41 @@ for name in new_names:
 
 df2 = df.set_axis(final_headers, axis=1)
 
-# print(df.columns)
+def nuclide_extractor(head):
+	element_name = ''
+	isotope_string = ''
+	for char in header:
+		if char == ' ':
+			break
+		try:
+			integer = int(char)
+			isotope_string += char
+		except ValueError:
+			element_name += char
+
+	isotope = int(isotope_string)
+	atomic_number = periodictable.elements.symbol(element_name).number
+	print(atomic_number, isotope)
+
+for header in df2.columns[1:]:
+	nuclide_extractor(head=header)
+
+
+
+
+
+mt102 = pd.DataFrame(columns=['Z', 'A', 'ERG', 'XS'])
+
+
+ERG = []
+for i, row in df2.iterrows():
+	# print(row)
+	if i > 1:
+		energy = float(row['Incident energy'].strip())
+		if energy
+		ERG.append(float(row['Incident energy'].strip()))
+
+
+
+
+
