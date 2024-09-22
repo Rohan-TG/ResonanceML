fixed scaling

AI_broadening/neural_broadening/single_nuclide_MLP_broadener.py
@@ -80,18 +80,18 @@ for pair in X_test:
 all_energies = df['ERG'].values
 all_xs = df['XS'].values
 
-max_all_energy = max(all_energies) * 1e6
-min_all_energy = min(all_energies) * 1e6
+max_all_energy = max(unscaled_energy) * 1e6
+min_all_energy = min(unscaled_energy) * 1e6
 
-max_all_xs = max(all_xs)
-min_all_xs = min(all_xs)
+max_all_xs = max(unscaled_xs)
+min_all_xs = min(unscaled_xs)
 
-rescaled_energies = np.array(scaled_energies)* (max_all_energy - min_all_energy)
+rescaled_energies = np.array(scaled_energies)* (max_all_energy - min_all_energy) + min_all_energy
 
-rescaled_test_xs = np.array(y_test) * (max_all_xs - min_all_xs)
+rescaled_test_xs = np.array(y_test) * (max_all_xs - min_all_xs) + min_all_xs
 
 
-rescaled_predictions = np.array(predictions) * (max_all_xs - min_all_xs)
+rescaled_predictions = np.array(predictions) * (max_all_xs - min_all_xs) + min_all_xs
 
 
 unheated_energies = df[(df['T'] == 0) & (df['ERG'] > (min_energy/1e6)) & (df['ERG'] < (max_energy/1e6))]['ERG'].values