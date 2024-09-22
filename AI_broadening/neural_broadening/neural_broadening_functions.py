bbb7e8a4a5308afb45c286e1331ab7a3e52c1999
RNTG <rohan.tg05@gmail.com>
2024-09-22 19:28:26 +0100

    still broken

--- AI_broadening/neural_broadening/neural_broadening_functions.py
+++ AI_broadening/neural_broadening/neural_broadening_functions.py
@@ -1,1 +1,169 @@
 import tqdm
+import numpy as np
+import scipy
+from sklearn.preprocessing import minmax_scale
+
+def single_nuclide_make_train(df, val_temperatures=[], test_temperatures=[], minERG=0, maxERG=30e6, use_tqdm = False):
+	"""Generates training data matrix"""
+
+	# XS = df['XS']
+	# ERG = df['ERG']
+	# T = df['T']
+
+	XS_train = []
+	ERG_train = []
+	T_train = []
+	# XS_0K_train = []
+
+
+
+	if use_tqdm:
+		iterator = tqdm.tqdm(df.iterrows(), total = len(df))
+	else:
+		iterator = df.iterrows()
+
+	for i, row in iterator:
+		if row['T'] in val_temperatures or row['T'] in test_temperatures:
+			continue
+		if (row['ERG'] * 1e6) > maxERG or (row['ERG'] * 1e6) < minERG:
+			continue
+		XS_train.append(row['XS'])
+		ERG_train.append(row['ERG'])
+		T_train.append(row['T'])
+
+	X = np.array([ERG_train, T_train])
+	ignore_list = []
+
+	for j_idx, feature_list in enumerate(X):
+		if j_idx not in ignore_list:
+			X[j_idx] = minmax_scale(feature_list)
+
+
+	y = np.array(XS_train)
+	y = minmax_scale(y)
+
+	X = np.transpose(X)
+
+	return X, y, ERG_train, XS_train
+
+
+
+def single_nuclide_make_test(df, test_temperatures = [], use_tqdm = False, minERG = 0, maxERG = 30e6):
+
+	XS_test = []
+	ERG_test = []
+	unscaled_energy = []
+	T_test = []
+
+	T = df['T'].values
+	Tscaled = minmax_scale(T)
+
+
+
+
+	if use_tqdm:
+		iterator = tqdm.tqdm(df.iterrows(), total = len(df))
+	else:
+		iterator = df.iterrows()
+
+	for i, row in iterator:
+		if (row['ERG'] * 1e6) > maxERG or (row['ERG'] * 1e6) < minERG:
+			continue
+		if row['T'] in test_temperatures:
+			XS_test.append(row['XS'])
+			ERG_test.append(row['ERG'])
+			T_test.append(Tscaled[i])
+			unscaled_energy.append(row['ERG'])
+		else:
+			continue
+
+	X = np.array([ERG_test, T_test])
+	ignore_list = [1]
+
+	for j_idx, feature_list in enumerate(X):
+		if j_idx not in ignore_list:
+			X[j_idx] = minmax_scale(feature_list)
+
+	y = np.array(XS_test)
+	y = minmax_scale(y)
+
+	X = np.transpose(X)
+
+
+
+	return X, y, unscaled_energy, XS_test
+
+
+
+
+
+
+
+
+
+
+def single_nuclide_make_val(df, val_temperatures = [], use_tqdm = False, minERG = 0, maxERG = 30e6):
+
+	XS_test = []
+	ERG_test = []
+	unscaled_energy = []
+	T_test = []
+
+	T = df['T'].values
+	Tscaled = minmax_scale(T)
+
+
+	if use_tqdm:
+		iterator = tqdm.tqdm(df.iterrows(), total = len(df))
+	else:
+		iterator = df.iterrows()
+
+	for i, row in iterator:
+		if (row['ERG'] * 1e6) > maxERG or (row['ERG'] * 1e6) < minERG:
+			continue
+		if row['T'] in val_temperatures:
+			XS_test.append(row['XS'])
+			ERG_test.append(row['ERG'])
+			T_test.append(Tscaled[i])
+			unscaled_energy.append(row['ERG'])
+		else:
+			continue
+
+	X = np.array([ERG_test, T_test])
+	ignore_list = [1]
+
+	for j_idx, feature_list in enumerate(X):
+		if j_idx not in ignore_list:
+			X[j_idx] = minmax_scale(feature_list)
+
+	y = np.array(XS_test)
+	y = minmax_scale(y)
+
+
+	X = np.transpose(X)
+
+
+
+	return X, y, unscaled_energy
+
+
+
+
+
+
+
+
+# def scaler(X_matrix):
+#
+# 	ignore_list = []
+#
+# 	X_matrix = X_matrix.transpose()
+# 	for j_idx, feature_list in enumerate(X_matrix):
+# 		if j_idx not in ignore_list:
+# 			X_matrix[j_idx] = scipy.stats.zscore(feature_list)
+#
+# 	means = []
+# 	stds = []
+# 	for feature in X_matrix:
+# 		means.append(np.mean(feature))
+# 		stds.append(np.std(feature))
---
+++ AI_broadening/neural_broadening/single_nuclide_MLP_broadenery.py
@@ -0,0 +1,112 @@
+import numpy as np
+import tensorflow as tf
+import keras
+import scipy
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+from neural_broadening_functions import single_nuclide_make_train, single_nuclide_make_test, single_nuclide_make_val
+import pandas as pd
+
+df =pd.read_csv('Fe56_MT_102_Delta100K_0K_1800K.csv')
+
+test_temperatures = [1800]
+validation_temperatures = [1700,
+						   1600,
+						   1500,
+						   1400,
+						   # 1300,
+						   # 1200,
+						   # 1100,
+						   ]
+nuclide = [26,56]
+
+
+min_energy = 100
+max_energy = 2e6
+X_train, y_train, unscaled_erg_train, unscaled_xs_train = single_nuclide_make_train(df=df,
+											 val_temperatures=validation_temperatures,
+											 test_temperatures=test_temperatures,
+											 minERG=min_energy,
+											 maxERG=max_energy,
+											 use_tqdm=True)
+
+X_test, y_test, unscaled_energy, unscaled_xs = single_nuclide_make_test(df=df,
+										  test_temperatures=test_temperatures,
+										  use_tqdm=True,
+										  minERG=min_energy,
+										  maxERG=max_energy)
+
+X_val, y_val, unscaled_val_energies = single_nuclide_make_val(df=df,
+										use_tqdm=True,
+										minERG=min_energy,
+										maxERG=max_energy,
+										val_temperatures=validation_temperatures)
+
+callback = keras.callbacks.EarlyStopping(monitor='val_loss',
+										 min_delta=0.01,
+										 patience=5,
+										 mode='min',
+										 start_from_epoch=3,
+										 restore_best_weights=True)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(10, input_shape=(X_train.shape[1],), kernel_initializer='normal', activation='relu'))
+model.add(keras.layers.Dense(10, activation='relu'))
+model.add(keras.layers.Dense(50,activation='relu'))
+model.add(keras.layers.Dense(1, activation='linear'))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+history = model.fit(X_train,
+					y_train,
+					epochs=5,
+					batch_size=32,
+					callbacks=callback,
+					validation_data=(X_val, y_val),
+					verbose=1)
+
+predictions = model.predict(X_test)
+predictions = predictions.ravel()
+
+
+
+scaled_energies = []
+for pair in X_test:
+	scaled_energies.append(pair[0])
+
+
+all_energies = df['ERG'].values
+all_xs = df['XS'].values
+
+max_all_energy = max(all_energies) * 1e6
+min_all_energy = min(all_energies) * 1e6
+
+max_all_xs = max(all_xs)
+min_all_xs = min(all_xs)
+
+rescaled_energies = np.array(scaled_energies)* (max_all_energy - min_all_energy)
+
+rescaled_test_xs = np.array(y_test) * (max_all_xs - min_all_xs)
+
+
+rescaled_predictions = np.array(predictions) * (max_all_xs - min_all_xs)
+
+
+unheated_energies = df[(df['T'] == 0) & (df['ERG'] > (min_energy/1e6)) & (df['ERG'] < (max_energy/1e6))]['ERG'].values
+unheated_energies = [e * 1e6 for e in unheated_energies]
+unheated_XS = df[(df['T'] == 0) & (df['ERG'] > (min_energy/1e6)) & (df['ERG'] < (max_energy/1e6))]['XS'].values
+
+
+plt.figure()
+plt.plot(rescaled_energies, rescaled_predictions, label = 'Predictions')
+plt.plot(rescaled_energies, rescaled_test_xs, label = 'JEFF-3.3 1,800 K')
+plt.plot(unheated_energies, unheated_XS, label = 'JEFF-3.3 0 K')
+plt.legend()
+plt.grid()
+plt.xlabel('Energy / eV')
+plt.ylabel('$\sigma_{n,\gamma} / b$')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
