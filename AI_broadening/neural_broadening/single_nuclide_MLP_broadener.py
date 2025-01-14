7953d869598687b4345640d6e0bc17faf544bf71
RNTG <rohan.tg05@gmail.com>
2025-01-14 11:49:58 +0000

    scaling kind of works for now

--- AI_broadening/neural_broadening/single_nuclide_MLP_broadener.py
+++ AI_broadening/neural_broadening/single_nuclide_MLP_broadener.py
@@ -7,10 +7,114 @@ import tqdm
 
 import pandas as pd
 
-min_energy = 0.00001
-max_energy = 1e4
+min_energy = 0.0000
+max_energy = 3e6
 
-def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use_tqdm=False, minERG=0, maxERG=30e6):
+# def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use_tqdm=False, minERG=0, maxERG=30e6):
+# 	XS_train = []
+# 	ERG_train = []
+# 	T_train = []
+#
+# 	XS_val = []
+# 	ERG_val = []
+# 	T_val = []
+#
+# 	XS_test = []
+# 	ERG_test = []
+# 	T_test = []
+#
+# 	if use_tqdm:
+# 		iterator = tqdm.tqdm(df.iterrows(), total=len(df))
+# 	else:
+# 		iterator = df.iterrows()
+#
+# 	for i, row in iterator:
+# 		if row['ERG'] > maxERG or row['ERG'] < minERG:
+# 			continue
+# 		if row['T'] in val_temperatures:
+# 			XS_val.append(row['XS'])
+# 			ERG_val.append(row['ERG'])
+# 			T_val.append(row['T'])
+# 		if row['T'] in test_temperatures:
+# 			XS_test.append(row['XS'])
+# 			ERG_test.append(row['ERG'])
+# 			T_test.append(row['T'])
+# 		if row['T'] not in val_temperatures and row['T'] not in test_temperatures:
+# 			XS_train.append(row['XS'])
+# 			ERG_train.append(row['ERG'])
+# 			T_train.append(row['T'])
+#
+# 	X_train = np.array([ERG_train, T_train])
+# 	y_train = np.array(XS_train)
+# 	y_train = scipy.stats.zscore(y_train)
+#
+# 	feature_means = []
+# 	feature_stds = []
+# 	for j_idx, feature_list in enumerate(X_train):
+# 		X_train[j_idx] = scipy.stats.zscore(feature_list)
+# 		feature_means.append(np.mean(feature_list))
+# 		feature_stds.append(np.std(feature_list))
+#
+# 	X_train = np.transpose(X_train)
+#
+# 	ERG_train_mean = np.mean(ERG_train)
+# 	ERG_train_std = np.std(ERG_train)
+#
+# 	T_train_mean = np.mean(T_train)
+# 	T_train_std = np.std(T_train)
+#
+# 	XS_train_mean = np.mean(XS_train)
+# 	XS_train_std = np.std(XS_train)
+#
+#
+#
+#
+#
+# 	scaled_ERG_val = []
+# 	scaled_T_val = []
+# 	scaled_XS_val = []
+#
+# 	for v in ERG_val:
+# 		scaled_ERG_val.append((v - ERG_train_mean) / ERG_train_std)
+#
+# 	for v in T_val:
+# 		scaled_T_val.append((v - T_train_mean) / T_train_std)
+#
+# 	for v in XS_val:
+# 		scaled_XS_val.append((v - XS_train_mean) / XS_train_std)
+#
+# 	X_val = np.array([scaled_ERG_val, scaled_T_val])
+# 	X_val = np.transpose(X_val)
+# 	y_val = np.array(scaled_XS_val)
+#
+#
+#
+#
+#
+#
+#
+# 	scaled_ERG_test = []
+# 	scaled_T_test = []
+# 	scaled_XS_test = []
+#
+# 	for v in ERG_test:
+# 		scaled_ERG_test.append((v - ERG_train_mean) / ERG_train_std)
+#
+# 	for v in T_test:
+# 		scaled_T_test.append((v - T_train_mean) / T_train_std)
+#
+# 	for v in XS_test:
+# 		scaled_XS_test.append((v - XS_train_mean) / XS_train_std)
+#
+# 	X_test = np.array([scaled_ERG_test, scaled_T_test])
+# 	X_test = np.transpose(X_test)
+# 	y_test = np.array(scaled_XS_test)
+#
+# 	return X_train, y_train, ERG_train, XS_train, X_val, y_val, ERG_val, XS_val, X_test, y_test, ERG_test, XS_test, feature_means, feature_stds
+
+
+
+def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use_tqdm=False, minERG=0.0, maxERG=30e6):
 	XS_train = []
 	ERG_train = []
 	T_train = []
@@ -57,14 +161,14 @@ def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use
 
 	X_train = np.transpose(X_train)
 
-	ERG_train_mean = np.mean(ERG_train)
-	ERG_train_std = np.std(ERG_train)
+	ERG_mean = np.mean(df['ERG'].values)
+	ERG_std = np.std(df['ERG'].values)
 
-	T_train_mean = np.mean(T_train)
-	T_train_std = np.std(T_train)
+	T_mean = np.mean(df['T'].values)
+	T_std = np.std(df['T'].values)
 
-	XS_train_mean = np.mean(XS_train)
-	XS_train_std = np.std(XS_train)
+	XS_mean = np.mean(df['XS'].values)
+	XS_std = np.std(df['XS'].values)
 
 
 
@@ -75,13 +179,13 @@ def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use
 	scaled_XS_val = []
 
 	for v in ERG_val:
-		scaled_ERG_val.append((v - ERG_train_mean) / ERG_train_std)
+		scaled_ERG_val.append((v - ERG_mean) / ERG_std)
 
 	for v in T_val:
-		scaled_T_val.append((v - T_train_mean) / T_train_std)
+		scaled_T_val.append((v - T_mean) / T_std)
 
 	for v in XS_val:
-		scaled_XS_val.append((v - XS_train_mean) / XS_train_std)
+		scaled_XS_val.append((v - XS_mean) / XS_std)
 
 	X_val = np.array([scaled_ERG_val, scaled_T_val])
 	X_val = np.transpose(X_val)
@@ -98,19 +202,23 @@ def single_nuclide_data_maker(df, val_temperatures=[], test_temperatures=[], use
 	scaled_XS_test = []
 
 	for v in ERG_test:
-		scaled_ERG_test.append((v - ERG_train_mean) / ERG_train_std)
+		scaled_ERG_test.append((v - ERG_mean) / ERG_std)
 
 	for v in T_test:
-		scaled_T_test.append((v - T_train_mean) / T_train_std)
+		scaled_T_test.append((v - T_mean) / T_std)
 
 	for v in XS_test:
-		scaled_XS_test.append((v - XS_train_mean) / XS_train_std)
+		scaled_XS_test.append((v - XS_mean) / XS_std)
 
 	X_test = np.array([scaled_ERG_test, scaled_T_test])
 	X_test = np.transpose(X_test)
 	y_test = np.array(scaled_XS_test)
 
 	return X_train, y_train, ERG_train, XS_train, X_val, y_val, ERG_val, XS_val, X_test, y_test, ERG_test, XS_test, feature_means, feature_stds
+
+
+
+
 
 
 df =pd.read_csv('Fe56_MT_102_Delta50K_0K_1800K.csv')
@@ -157,8 +265,8 @@ model.add(keras.layers.Dropout(0.05))
 # model.add(keras.layers.Dense(1000, activation='relu'))
 # model.add(keras.layers.Dense(1000, activation='relu'))
 # model.add(keras.layers.Dropout(0.05))
-# model.add(keras.layers.Dense(1000, activation='relu'))
 model.add(keras.layers.Dense(1000, activation='relu'))
+# model.add(keras.layers.Dense(1000, activation='relu'))
 model.add(keras.layers.Dropout(0.05))
 model.add(keras.layers.Dense(1000, activation='relu'))
 model.add(keras.layers.Dense(600,activation='relu'))
@@ -206,18 +314,18 @@ for pair in X_test:
 # rescaled_predictions = np.array(predictions) * (max_test_xs - min_test_xs) + min_test_xs
 
 
-rescaled_energies = 1e6 * np.array(scaled_energies) * np.std(ERG_train) + np.mean(ERG_train) * 1e6
+rescaled_energies = np.array(scaled_energies) * np.std(df['ERG'].values) + np.mean(df['ERG'].values)
 
-rescaled_predictions = np.array(predictions) * np.std(XS_train) + np.mean(XS_train)
+# rescaled_predictions = np.array(predictions) * np.std(df['XS'].values) + np.mean(df['XS'].values)
 
-rescaled_test_xs = np.array(y_test) * np.std(XS_train) + np.mean(XS_train)
+rescaled_test_xs = np.array(y_test) * np.std(df['XS'].values) + np.mean(df['XS'].values)
 
 
 
 
 
 plt.figure()
-plt.plot(rescaled_energies, rescaled_predictions, label = 'Predictions', color = 'red')
+# plt.plot(rescaled_energies, rescaled_predictions, label = 'Predictions', color = 'red')
 plt.plot(unheated_energies, unheated_XS, label = 'JEFF-3.3 0 K')
 plt.plot(rescaled_energies, rescaled_test_xs, '--', label = 'JEFF-3.3 1,800 K')
 plt.legend()
