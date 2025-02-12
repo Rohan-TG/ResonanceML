5debcfc53601838e10ed55b11081e179975121ce
RNTG <rohan.tg05@gmail.com>
2025-02-12 13:17:14 +0000

    wip

--- AI_broadening/neural_broadening/single_nuclide_MLP_broadener.py
+++ AI_broadening/neural_broadening/single_nuclide_MLP_broadener.py
@@ -7,14 +7,16 @@ import tqdm
 from neural_broadening_functions import single_nuclide_data_maker
 import pandas as pd
 
-min_energy = 800
-max_energy = 1500
+minerg = 800
+maxerg = 1500
 
 
 all_temperatures = np.arange(0, 1801, 20) # all temperatures in the data file
 
 
-df = pd.read_csv('Fe56_MT_102_eV_0K_to_4000K_Delta20K.csv')
+df = pd.read_csv('Fe56_200_to_1800_D1K_MT102.csv')
+
+df = df[(df['ERG'] < maxerg) & (df['ERG'] > minerg)]
 
 # df = pd.read_csv('Fe56_200_to_1800_D1K.MT102.csv')
 
@@ -25,28 +27,36 @@ nuclide = [26,56]
 
 
 
-unheated_energies = df[(df['T'] == 0) & (df['ERG'] > min_energy) & (df['ERG'] < max_energy)]['ERG'].values
-unheated_XS = df[(df['T'] == 0) & (df['ERG'] > min_energy) & (df['ERG'] < max_energy)]['XS'].values
+df0 = pd.read_csv('Fe56_MT_102_eV_0K_to_4000K_Delta20K.csv')
+unheated_energies = df0[(df0['T'] == 0) & (df0['ERG'] > minerg) & (df0['ERG'] < maxerg)]['ERG'].values
+unheated_XS = df0[(df0['T'] == 0) & (df0['ERG'] > minerg) & (df0['ERG'] < maxerg)]['XS'].values
 
 X_train, y_train, ERG_train, XS_train, X_val, y_val, ERG_val, XS_val, X_test, y_test, ERG_test, feature_means, feature_stds = single_nuclide_data_maker(df, val_temperatures=validation_temperatures,
 																																								 test_temperatures=test_temperatures,
 																																								 use_tqdm=True,
-																																								 minERG=min_energy,
-																																								 maxERG=max_energy,)
+																																								 minERG=minerg,
+																																								 maxERG=maxerg,)
 
 
 
 
 callback = keras.callbacks.EarlyStopping(monitor='val_loss',
-										 min_delta=0.00005,
-										 patience=100,
+										 # min_delta=0.005,
+										 patience=10,
 										 mode='min',
-										 start_from_epoch=10,
+										 start_from_epoch=5,
 										 restore_best_weights=True)
 
 model = keras.Sequential()
-model.add(keras.layers.Dense(100, input_shape=(X_train.shape[1],), kernel_initializer='normal', activation='relu'))
-# model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.LeakyReLU(alpha=0.05))
+model.add(keras.layers.Dense(200))
+model.add(keras.layers.LeakyReLU(alpha=0.05))
+model.add(keras.layers.Dense(200))
+model.add(keras.layers.LeakyReLU(alpha=0.05))
+model.add(keras.layers.Dense(200))
+model.add(keras.layers.LeakyReLU(alpha=0.05))
+model.add(keras.layers.Dense(200))
 model.add(keras.layers.LeakyReLU(alpha=0.05))
 # model.add(keras.layers.Dense(1000, activation='relu'))
 # model.add(keras.layers.Dropout(0.05))
@@ -63,7 +73,7 @@ model.add(keras.layers.LeakyReLU(alpha=0.05))
 # model.add(keras.layers.Dense(300, activation='relu'))
 # model.add(keras.layers.Dense(300,activation='relu'))
 model.add(keras.layers.Dense(1, activation='linear'))
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='mean_absolute_error', optimizer='adam')
 
 history = model.fit(X_train,
 					y_train,
