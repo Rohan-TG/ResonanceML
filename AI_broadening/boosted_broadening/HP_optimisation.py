9f2a99102280bb4d589a25c1cc0412191c6a91a1
Rohan-TG <rohan.tg05@gmail.com>
2025-02-22 13:00:16 +0000

    increased cores

--- AI_broadening/boosted_broadening/HP_optimisation.py
+++ AI_broadening/boosted_broadening/HP_optimisation.py
@@ -1,10 +1,10 @@
 # Bayesian HP optimisation script
 import os
-os.environ["OMP_NUM_THREADS"] = "20"
-os.environ["MKL_NUM_THREADS"] = "20"
-os.environ["OPENBLAS_NUM_THREADS"] = "20"
-os.environ["TF_NUM_INTEROP_THREADS"] = "20"
-os.environ["TF_NUM_INTRAOP_THREADS"] = "20"
+os.environ["OMP_NUM_THREADS"] = "25"
+os.environ["MKL_NUM_THREADS"] = "25"
+os.environ["OPENBLAS_NUM_THREADS"] = "25"
+os.environ["TF_NUM_INTEROP_THREADS"] = "25"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "25"
 
 
 import random
