5ed0d46d28f47aa629edf95c3a66c3404bd68168
Rohan-TG <rohan.tg05@gmail.com>
2025-02-23 15:22:58 +0000

    imports

--- AI_broadening/neural_broadening/mlp_tuning.py
+++ AI_broadening/neural_broadening/mlp_tuning.py
@@ -1,3 +1,27 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "20"
+os.environ["MKL_NUM_THREADS"] = "20"
+os.environ["OPENBLAS_NUM_THREADS"] = "20"
+os.environ["TF_NUM_INTEROP_THREADS"] = "20"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "20"
+
+import hyperopt.early_stop
+from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
+import pickle
+from sklearn.metrics import mean_absolute_error
 import keras
 import numpy as np
 import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
+def build_model(hp):
+	pass
