b5dbbe9fe717e3ca3df2223680a6d20a545f4172
Rohan-TG <rohan.tg05@gmail.com>
2025-02-26 14:09:32 +0000

    thing

--- AI_broadening/neural_broadening/MLP_single_log_transform.py
+++ AI_broadening/neural_broadening/MLP_single_log_transform.py
@@ -274,7 +274,7 @@ plt.xlabel('Energy / eV')
 plt.ylabel('$\sigma_{n,\gamma} / b$')
 plt.xscale('log')
 plt.yscale('log')
-plt.savefig(f'mlpplot-{timestring}.png', dpi = 300)
+plt.savefig(f'mlpplot-{timestring}_fix.png', dpi = 300)
 plt.show()
 
 plt.figure()
