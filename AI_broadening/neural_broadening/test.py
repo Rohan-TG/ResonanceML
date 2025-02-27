bcc772dc768b7170715445838074efffeec8656e
Rohan-TG <rohan.tg05@gmail.com>
2025-02-27 20:24:45 +0000

    test h5

---
+++ AI_broadening/neural_broadening/test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import h5py
+import dask.dataframe
+
+
+# df = pd.read_hdf('../AI_data/capture_xs_data_39.h5')
+
+df = dask.dataframe.read_hdf('../AI_data/capture_xs_data_39.h5', key='xs_data')
+
+print(df)
+
+with h5py.File("../AI_data/capture_xs_data_39.h5", "r") as f:
+	# dataset = f["my_data"]
+	# print("Keys in the file:", list(f.keys()))
+	# dataset = f["xs_data"][:]  # Read the entire dataset into memory
+	print(f)
