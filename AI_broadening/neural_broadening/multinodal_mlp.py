8b65b826557f831ea590983c47d692f212d36a79
Rohan-TG <rohan.tg05@gmail.com>
2025-03-02 21:32:36 +0000

    bounds function working

--- AI_broadening/neural_broadening/multinodal_mlp.py
+++ AI_broadening/neural_broadening/multinodal_mlp.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import tqdm
+import periodictable
 
 # from neural_broadening_functions import log_single_nuclide_data_maker
 nuclide = [26,56]
@@ -41,6 +42,47 @@ unheated_XS = df0[(df0['T'] == 0) & (df0['ERG'] > minerg) & (df0['ERG'] < maxerg
 
 
 
+
+# def dataMaker(temperatures):
+#
+# 	input_matrix = [] # contains the energy grid and temperatures
+# 	labels_matrix = [] # contains the cross sections
+# 	for T in tqdm.tqdm(temperatures, total = len(temperatures)):
+# 		filestring = f'Fe56_T{int(T)}K.csv'
+# 		df = pd.read_csv(f'{data_dir}/{filestring}')
+# 		df = df[(df['ERG'] < maxerg) & (df['ERG'] > minerg)]
+#
+# 		unscaled_T_values = df['T'].values
+# 		scaled_T_values = [(t - mean_alltemps) / std_alltemps for t in unscaled_T_values]
+#
+# 		unscaled_ERG = df['ERG'].values
+# 		mean_ERG = np.mean(unscaled_ERG)
+# 		std_ERG = np.std(unscaled_ERG)
+# 		scaled_ERG = [(E - mean_ERG) / std_ERG for E in unscaled_ERG]
+#
+# 		input_submatrix = np.array([scaled_ERG, scaled_T_values]) # can add or remove ERG here to make energy an input parameter
+# 		labelsubmatrix = np.array(df['XS'].values)
+#
+# 		input_matrix.append(input_submatrix)
+# 		labels_matrix.append(labelsubmatrix)
+#
+# 	X = np.array(input_matrix)
+# 	y = np.array(labels_matrix)
+#
+# 	flattened_y = y.flatten()
+# 	meanXS = np.mean(flattened_y)
+# 	stdXS = np.std(flattened_y)
+#
+# 	scaled_labels_matrix = []
+# 	for set in labels_matrix:
+# 		scaled_set = [(xs - meanXS) / stdXS for xs in set]
+# 		scaled_labels_matrix.append(scaled_set)
+#
+# 	y = np.array(scaled_labels_matrix)
+#
+# 	return(X, y)
+
+
 def dataMaker(temperatures):
 
 	input_matrix = [] # contains the energy grid and temperatures
@@ -54,11 +96,11 @@ def dataMaker(temperatures):
 		scaled_T_values = [(t - mean_alltemps) / std_alltemps for t in unscaled_T_values]
 
 		unscaled_ERG = df['ERG'].values
-		mean_ERG = np.mean(unscaled_ERG)
-		std_ERG = np.std(unscaled_ERG)
-		scaled_ERG = [(E - mean_ERG) / std_ERG for E in unscaled_ERG]
+		# mean_ERG = np.mean(unscaled_ERG)
+		# std_ERG = np.std(unscaled_ERG)
+		# scaled_ERG = [(E - mean_ERG) / std_ERG for E in unscaled_ERG]
 
-		input_submatrix = np.array([scaled_ERG, scaled_T_values]) # can add or remove ERG here to make energy an input parameter
+		input_submatrix = np.array(scaled_T_values) # can add or remove ERG here to make energy an input parameter
 		labelsubmatrix = np.array(df['XS'].values)
 
 		input_matrix.append(input_submatrix)
@@ -82,9 +124,6 @@ def dataMaker(temperatures):
 
 
 
-
-
-
 X_val, y_val = dataMaker(temperatures=validation_temperatures)
 X_train, y_train = dataMaker(temperatures=training_temperatures)
 X_test, y_test = dataMaker(temperatures=test_temperatures)
@@ -92,104 +131,131 @@ X_test, y_test = dataMaker(temperatures=test_temperatures)
 
 
 
-# for trainT in tqdm.tqdm(training_temperatures, total = len(training_temperatures)):
-# 	filestring = f'Fe56_T{int(trainT)}K.csv'
-# 	dftrain = pd.read_csv(f'{data_dir}/{filestring}')
-# 	dftrain = dftrain[(dftrain['ERG'] < maxerg) & (dftrain['ERG'] > minerg)]
-#
-# 	unscaled_train_T = dftrain['T'].values
-# 	scaled_train_T = [(t - mean_alltemps) / std_alltemps for t in unscaled_train_T]
-#
-# 	unscaled_ERG = dftrain['ERG'].values
-# 	mean_ERG = np.mean(unscaled_ERG)
-# 	std_ERG = np.std(unscaled_ERG)
-# 	scaled_ERG_train = [(E - mean_ERG) / std_ERG for E in unscaled_ERG]
-#
-# 	Train_submatrix = np.array([scaled_ERG_train, scaled_train_T])  # can add or remove ERG here to make energy an input parameter
-# 	labelsubmatrixtrain = np.array(dftrain['XS'].values)
-#
-# 	X_train.append(Train_submatrix)
-# 	y_train.append(labelsubmatrixtrain)
-
-# X_train = np.array(X_train)
-# y_train = np.array(y_train)
-#
-# flattened_y_train = y_train.flatten()
-# meantrainXS = np.mean(flattened_y_train)
-# stdtrainXS = np.std(flattened_y_train)
-#
-# scaled_labels_validation_matrix = []
-# for trainset in labels_validation_matrix:
-# 	scaled_set = [(xs - meantrainXS) / stdtrainXS for xs in trainset]
-# 	scaled_labels_validation_matrix.append(scaled_set)
-#
-# y_train = np.array(scaled_labels_validation_matrix)
 
 
 
 
+callback = keras.callbacks.EarlyStopping(monitor='val_loss',
+										 # min_delta=0.005,
+										 patience=10,
+										 mode='min',
+										 start_from_epoch=5,
+										 restore_best_weights=True)
 
-# for testT in tqdm.tqdm(test_temperatures, total = len(test_temperatures)):
-# 	filestring = f'Fe56_T{int(testT)}K.csv'
-# 	dftrain = pd.read_csv(f'{data_dir}/{filestring}')
-# 	dftrain = dftrain[(dftrain['ERG'] < maxerg) & (dftrain['ERG'] > minerg)]
-#
-# 	unscaled_train_T = dftrain['T'].values
-# 	scaled_train_T = [(t - mean_alltemps) / std_alltemps for t in unscaled_train_T]
-#
-# 	unscaled_ERG = dftrain['ERG'].values
-# 	mean_ERG = np.mean(unscaled_ERG)
-# 	std_ERG = np.std(unscaled_ERG)
-# 	scaled_ERG_train = [(E - mean_ERG) / std_ERG for E in unscaled_ERG]
-#
-# 	Train_submatrix = np.array([scaled_ERG_train, scaled_train_T])  # can add or remove ERG here to make energy an input parameter
-# 	labelsubmatrixtrain = np.array(dftrain['XS'].values)
-#
-# 	X_train.append(Train_submatrix)
-# 	y_train.append(labelsubmatrixtrain)
+model = keras.Sequential()
+model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(y_test.shape[1], activation='linear'))
+model.compile(loss='mean_absolute_error', optimizer='adam')
 
-# X_train = np.array(X_train)
-# y_train = np.array(y_train)
-#
-# flattened_y_train = y_train.flatten()
-# meantrainXS = np.mean(flattened_y_train)
-# stdtrainXS = np.std(flattened_y_train)
-#
-# scaled_labels_validation_matrix = []
-# for trainset in labels_validation_matrix:
-# 	scaled_set = [(xs - meantrainXS) / stdtrainXS for xs in trainset]
-# 	scaled_labels_validation_matrix.append(scaled_set)
-#
-# y_train = np.array(scaled_labels_validation_matrix)
+history = model.fit(X_train,
+					y_train,
+					epochs=30,
+					batch_size=64,
+					callbacks=callback,
+					validation_data=(X_train, y_train),
+					verbose=1)
 
 
+predictions = model.predict(X_test)
+predictions = predictions.ravel()
 
+teststring = f'Fe56_T{int(test_temperatures[0])}K.csv'
+dftest = pd.read_csv(f'{data_dir}/{teststring}')
+dftest = dftest[(dftest['ERG'] < maxerg) & (dftest['ERG'] > minerg)]
 
+testxs = dftest['XS'].values
+meantestxs = np.mean(testxs)
+stdtestxs = np.std(testxs)
 
+rescaled_predictions = [p * stdtestxs + meantestxs for p in predictions]
+energies = dftest['ERG'].values
 
 
 
-# callback = keras.callbacks.EarlyStopping(monitor='val_loss',
-# 										 # min_delta=0.005,
-# 										 patience=10,
-# 										 mode='min',
-# 										 start_from_epoch=5,
-# 										 restore_best_weights=True)
+
+def bounds(lower_bound, upper_bound, scalex='log', scaley='log'):
+	unheated_energies_limited = []
+	unheated_XS_limited = []
+	for x, h in zip(unheated_energies, unheated_XS):
+		if x <= upper_bound and x >= lower_bound:
+			unheated_energies_limited.append(x)
+			unheated_XS_limited.append(h)
+
+	test_energies_limited = []
+	predictions_limited = []
+	test_XS_limited = []
+	for o, p, qx in zip(energies, rescaled_predictions, testxs):
+		if o <= upper_bound and o >= lower_bound:
+			test_energies_limited.append(o)
+			predictions_limited.append(p)
+			test_XS_limited.append(qx)
+
+	plt.figure()
+	plt.plot(unheated_energies_limited, unheated_XS_limited, label = '0 K JEFF-3.3')
+	plt.grid()
+	plt.plot(test_energies_limited, predictions_limited, label='Predictions', color='red')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('$\sigma_{n,\gamma} / b$')
+	plt.plot(test_energies_limited, test_XS_limited, '--', label=f'{test_temperatures[0]} K JEFF-3.3', color='lightgreen',
+			 alpha=0.7)
+	plt.legend()
+	plt.xscale('log')
+	plt.yscale('log')
+	plt.title(f'{periodictable.elements[nuclide[0]]}-{nuclide[1]} $\sigma_{{n,\gamma}}$ at {test_temperatures[0]} K')
+	plt.show()
+
+
+	relativeError = []
+	percentageError = []
+	for p, xs in zip(predictions_limited, test_XS_limited):
+		relativeError.append(abs((p-xs)/xs))
+		percentageError.append((p/xs * 100) - 100)
+
+
+
+	plt.figure()
+	plt.plot(test_energies_limited, relativeError, label = 'Error')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('Relative error')
+	plt.xscale('log')
+	plt.legend()
+	plt.yscale('log')
+	plt.grid()
+	plt.show()
+
+	plt.figure()
+	plt.plot(test_energies_limited, percentageError, label='Error')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('% Error')
+	plt.grid()
+	plt.show()
+
+	plt.figure()
+	plt.hist(percentageError, bins=50)
+	plt.ylabel('Frequency')
+	plt.xlabel('% Error')
+	plt.grid()
+	plt.show()
+
+	countoverthreshold = 0
+	for XX in percentageError:
+		if XX >= 0.1:
+			countoverthreshold += 1
+
+	percentageOverThreshold = (countoverthreshold / (len(percentageError))) * 100
+
+	print(f'Max error: {np.max(abs(np.array(percentageError)))}')
+	print(f'Mean error: {np.mean(abs(np.array(percentageError)))}')
+	print(f'{percentageOverThreshold} % of points over limit of 0.1 % error')
+
 #
-# model = keras.Sequential()
-# model.add(keras.layers.Dense(200, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
-# model.add(keras.layers.Dense(100, activation='relu'))
-# model.add(keras.layers.Dense(y_test.shape[0], activation='linear'))
-# model.compile(loss='mean_absolute_error', optimizer='adam')
-#
-# history = model.fit(X_train,
-# 					y_train,
-# 					epochs=30,
-# 					batch_size=64,
-# 					callbacks=callback,
-# 					validation_data=(X_train, y_train),
-# 					verbose=1)
-#
-#
-# predictions = model.predict(X_train)
-# predictions = predictions.ravel()
+# plt.figure()
+# plt.plot(unheated_energies, unheated_XS, color = 'lightgreen', label = '0 K')
+# plt.plot(energies, testxs, '--', label = 'JEFF-3.3')
+# plt.plot(energies, rescaled_predictions, color = 'red', label = 'Predictions')
+# plt.legend()
+# plt.yscale('log')
+# plt.xscale('log')
+# plt.show()
