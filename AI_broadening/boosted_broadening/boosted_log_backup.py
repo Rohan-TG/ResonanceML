6a334acf1693ff26e8cb78f2e492071ef3a271af
Rohan-TG <rohan.tg05@gmail.com>
2025-03-06 23:13:10 +0000

    data scaling

--- AI_broadening/boosted_broadening/boosted_log_backup.py
+++ AI_broadening/boosted_broadening/boosted_log_backup.py
@@ -13,7 +13,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import periodictable
 import numpy as np
-import tqdm
+import scipy
 
 
 
@@ -28,7 +28,7 @@ def bounds(lower_bound, upper_bound, scalex='log', scaley='log'):
 	test_energies_limited = []
 	predictions_limited = []
 	test_XS_limited = []
-	for o, p, qx in zip(rescaled_test_energies, rescaled_predictions, rescaled_test_XS):
+	for o, p, qx in zip(rescaled_energies, rescaled_predictions, rescaled_test_xs):
 		if o <= upper_bound and o >= lower_bound:
 			test_energies_limited.append(o)
 			predictions_limited.append(p)
@@ -134,15 +134,44 @@ nuclide = [26,56]
 
 
 
+validation_temperatures = []
+while len(validation_temperatures) < int(len(all_temperatures) * 0.1):
+	choice = random.choice(all_temperatures)
+	if choice not in validation_temperatures and choice not in test_temperatures:
+		validation_temperatures.append(choice)
+
+training_temperatures = []
+for T in all_temperatures:
+	if T not in test_temperatures and T not in validation_temperatures:
+		training_temperatures.append(T)
+
+
+mean_alltemps = np.mean(all_temperatures)
+std_alltemps = np.std(all_temperatures)
+
 test_dataframe = df[df['T'].isin(test_temperatures)]
 training_dataframe = df[df['T'].isin(training_temperatures)]
-X_train = np.array([np.log(training_dataframe['ERG'].values), training_dataframe['T'].values])
+logged_T_train = np.log(training_dataframe['T'].values)
+scaled_T_train = [(x - mean_alltemps) / std_alltemps for x in logged_T_train]
+logged_ERG_train = np.log(training_dataframe['ERG'].values)
+X_train = np.array([scipy.stats.zscore(logged_ERG_train), scaled_T_train])
 X_train = np.transpose(X_train)
-y_train = np.array(np.log(training_dataframe['XS'].values))
+y_train_logged = np.array(np.log(training_dataframe['XS'].values))
+y_train = scipy.stats.zscore(y_train_logged)
 
-X_test = np.array([np.log(test_dataframe['ERG'].values), test_dataframe['T'].values])
+
+
+
+logged_T_test = np.log(test_dataframe['T'].values)
+scaled_T_test = [(x - mean_alltemps) / std_alltemps for x in logged_T_test]
+
+ERG_test = test_dataframe['ERG'].values
+logged_ERG_test = np.log(ERG_test)
+X_test = np.array([scipy.stats.zscore(logged_ERG_test), scaled_T_test])
 X_test = np.transpose(X_test)
-y_test = np.array(np.log(test_dataframe['XS'].values))
+logged_y_test = np.log(np.array(test_dataframe['XS'].values))
+y_test = scipy.stats.zscore(logged_y_test)
+
 
 def log_loss_obj(y_pred, dtrain):
 	y_true = dtrain.get_label()
@@ -154,12 +183,12 @@ def log_loss_obj(y_pred, dtrain):
 
 
 
-model = xg.XGBRegressor(n_estimators = 14900,
-						max_depth = 17,
-						learning_rate = 0.019285510280325116,
-						reg_lambda = 65.93572569404694,
-						subsample = 0.37208217263358057,
-						gamma = 1.0362618958494487
+model = xg.XGBRegressor(n_estimators = 10450,
+						max_depth = 16,
+						learning_rate = 0.25919607000481934,
+						reg_lambda = 2.415057075497998,
+						subsample = 0.13021504261911765,
+						gamma = 1.036
 						)
 
 
@@ -174,25 +203,38 @@ predictions = model.predict(X_test)
 history = model.evals_result()
 
 
-test_energies = X_test.transpose()[0]
+# test_energies = X_test.transpose()[0]
 # test_energies = [e * 1e6 for e in test_energies]
 
 unheated_energies = df0[(df0['T'] == 0) & (df0['ERG'] > (minerg)) & (df0['ERG'] < (maxerg))]['ERG'].values
 unheated_energies = [e for e in unheated_energies]
 unheated_XS = df0[(df0['T'] == 0) & (df0['ERG'] > (minerg)) & (df0['ERG'] < (maxerg))]['XS'].values
 
-rescaled_test_energies = [np.e ** E for E in test_energies]
-rescaled_test_XS = [np.e ** XS for XS in y_test]
+scaled_energies = []
+for pair in X_test:
+	scaled_energies.append(pair[0])
 
-rescaled_predictions = [np.e ** p for p in predictions]
+rescaled_energies = np.array(scaled_energies) * np.std(logged_ERG_test) + np.mean(logged_ERG_test)
+rescaled_energies = np.e ** rescaled_energies
+
+rescaled_predictions = np.array(predictions) * np.std(logged_y_test) + np.mean(logged_y_test)
+rescaled_predictions = np.e ** rescaled_predictions
+
+rescaled_test_xs = np.array(y_test) * np.std(logged_y_test) + np.mean(logged_y_test)
+
+
+# rescaled_test_energies = [np.e ** E for E in test_energies]
+rescaled_test_xs = [np.e ** XS for XS in rescaled_test_xs]
+
+# rescaled_predictions = [np.e ** p for p in predictions]
 
 plt.figure()
 plt.plot(np.array(unheated_energies), np.array(unheated_XS), label = '0 K JEFF-3.3')
 plt.grid()
-plt.plot(np.array(rescaled_test_energies), np.array(rescaled_predictions), label = 'Predictions', color = 'red')
+plt.plot(np.array(rescaled_energies), np.array(rescaled_predictions), label = 'Predictions', color = 'red')
 plt.xlabel('Energy / eV')
 plt.ylabel('$\sigma_{n,\gamma} / b$')
-plt.plot(np.array(rescaled_test_energies), np.array(rescaled_test_XS), '--', label = f'{test_temperatures[0]} K JEFF-3.3', color = 'lightgreen')
+plt.plot(np.array(rescaled_energies), np.array(rescaled_test_xs), '--', label = f'{test_temperatures[0]} K JEFF-3.3', color = 'lightgreen')
 plt.legend()
 plt.title(f'{periodictable.elements[nuclide[0]]}-{nuclide[1]} $\sigma_{{n,\gamma}}$ at {test_temperatures[0]} K')
 plt.yscale('log')
