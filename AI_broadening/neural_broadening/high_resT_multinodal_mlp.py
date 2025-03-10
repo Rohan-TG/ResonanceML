337b4fb076bfd4bef9a265f75e720b79b9dfcd3d
Rohan-TG <rohan.tg05@gmail.com>
2025-03-10 13:07:08 +0000

    AAAAAAHHHHHHHH

--- AI_broadening/neural_broadening/high_resT_multinodal_mlp.py
+++ AI_broadening/neural_broadening/high_resT_multinodal_mlp.py
@@ -143,7 +143,7 @@ history = model.fit(X_train,
 predictions = model.predict(X_test)
 predictions = predictions.ravel()
 
-teststring = f'Fe56_{int(test_temperatures[0])}.0.csv'
+teststring = f'Fe_56_{int(test_temperatures[0])}.0.csv'
 dftest = pd.read_csv(f'{data_dir}/{teststring}')
 dftest = dftest[(dftest['ERG'] < maxerg) & (dftest['ERG'] > minerg)]
 
