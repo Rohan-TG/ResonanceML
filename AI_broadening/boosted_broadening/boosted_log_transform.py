7ac15e9a4f2b795069930039075389d50468284a
Rohan-TG <rohan.tg05@gmail.com>
2025-04-18 12:01:11 +0100

    increased learning rate

--- AI_broadening/boosted_broadening/boosted_log_transform.py
+++ AI_broadening/boosted_broadening/boosted_log_transform.py
@@ -158,11 +158,17 @@ def log_loss_obj(y_pred, dtrain):
 
 model = xg.XGBRegressor(n_estimators = 50450,
 						max_depth = 6,
-						learning_rate = 0.0025919607000481934,
+						learning_rate = 0.0085919607000481934,
 						reg_lambda = 2.415057075497998,
-						subsample = 0.13021504261911765,
+						subsample = 0.2021504261911765,
 						)
 
+# model = xg.XGBRegressor(n_estimators = 10450,
+# 						max_depth = 16,
+# 						learning_rate = 0.0025919607000481934,
+# 						reg_lambda = 2.415057075497998,
+# 						subsample = 0.13021504261911765,
+# 						)
 
 model.fit(X_train, y_train, verbose = True,
 		  eval_set = [(X_train, y_train),
