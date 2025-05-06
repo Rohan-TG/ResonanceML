8dd372351ecb91fd601270ae90d1931b42a19d73
Rohan-TG <rohan.tg05@gmail.com>
2025-05-06 09:04:56 -0500

    core usage

--- AI_broadening/neural_broadening/tenthK_continuous_mlp.py
+++ AI_broadening/neural_broadening/tenthK_continuous_mlp.py
@@ -1,10 +1,17 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "30"
+os.environ["MKL_NUM_THREADS"] = "30"
+os.environ["OPENBLAS_NUM_THREADS"] = "30"
+os.environ["TF_NUM_INTEROP_THREADS"] = "30"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "30"
+
 import pandas as pd
 import keras
 import datetime
 import numpy as np
 import random
 import periodictable
-import os
 import itertools
 import scipy
 import matplotlib.pyplot as plt
