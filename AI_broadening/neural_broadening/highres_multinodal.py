a9b02596cb794451e3b20be2b744750541224b4b
Rohan-TG <rohan.tg05@gmail.com>
2025-05-22 22:32:07 +0200

    attempt 1

--- AI_broadening/neural_broadening/highres_multinodal.py
+++ AI_broadening/neural_broadening/highres_multinodal.py
@@ -21,6 +21,8 @@ from sklearn.metrics import mean_absolute_error
 def get_datetime_string():
 	return datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
 
+nuclide = [26, 56]
+
 maxtemp = 1200
 mintemp = 1000
 
@@ -175,3 +177,170 @@ model.add(keras.layers.Dense(y_test.shape[1]))
 model.add(keras.layers.LeakyReLU(alpha=0.2))
 model.add(keras.layers.Dense(y_test.shape[1], activation='linear'))
 model.compile(loss='mean_absolute_error', optimizer='adam')
+
+
+
+
+# new additions
+
+history = model.fit(X_train,
+					y_train,
+					epochs=200,
+					batch_size=16,
+					callbacks=callback,
+					validation_data=(X_val, y_val),
+					verbose=1)
+
+
+predictions = model.predict(X_test)
+predictions = predictions.ravel()
+
+teststring = f'Fe_56_{int(test_temperatures[0])}.0.csv'
+dftest = pd.read_csv(f'{data_dir}/{teststring}')
+dftest = dftest[(dftest['ERG'] < maxerg) & (dftest['ERG'] > minerg)]
+
+testxs = dftest['XS'].values
+meantestxs = np.mean(np.log10(testxs))
+stdtestxs = np.std(np.log10(testxs))
+
+rescaled_predictions = [p * stdtestxs + meantestxs for p in predictions]
+energies = dftest['ERG'].values
+
+rescaled_predictions = [10 ** P for P in rescaled_predictions]
+
+
+def bounds(lower_bound, upper_bound, scalex='log', scaley='log'):
+	unheated_energies_limited = []
+	unheated_XS_limited = []
+	for x, h in zip(unheated_energies, unheated_XS):
+		if x <= upper_bound * 1e6 and x >= lower_bound * 1e6:
+			unheated_energies_limited.append(x)
+			unheated_XS_limited.append(h)
+
+	test_energies_limited = []
+	predictions_limited = []
+	test_XS_limited = []
+	for o, p, qx in zip(energies, rescaled_predictions, testxs):
+		if o <= upper_bound and o >= lower_bound:
+			test_energies_limited.append(o)
+			predictions_limited.append(p)
+			test_XS_limited.append(qx)
+
+	plt.figure()
+	plt.plot(unheated_energies_limited, unheated_XS_limited, label = '0 K JEFF-3.3')
+	plt.grid()
+	plt.plot(test_energies_limited, predictions_limited, label='Predictions', color='red')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('$\sigma_{n,\gamma} / b$')
+	plt.plot(test_energies_limited, test_XS_limited, '--', label=f'{test_temperatures[0]} K JEFF-3.3', color='lightgreen',
+			 alpha=0.7)
+	plt.legend()
+	plt.xscale('log')
+	plt.yscale('log')
+	plt.title(f'{periodictable.elements[nuclide[0]]}-{nuclide[1]} $\sigma_{{n,\gamma}}$ at {test_temperatures[0]} K')
+	plt.show()
+
+
+	relativeError = []
+	percentageError = []
+	for p, xs in zip(predictions_limited, test_XS_limited):
+		relativeError.append(abs((p-xs)/xs))
+		percentageError.append((p/xs * 100) - 100)
+
+
+
+	plt.figure()
+	plt.plot(test_energies_limited, relativeError, label = 'Error')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('Relative error')
+	plt.xscale('log')
+	plt.legend()
+	plt.yscale('log')
+	plt.grid()
+	plt.show()
+
+	plt.figure()
+	plt.plot(test_energies_limited, percentageError, label='Error')
+	plt.xlabel('Energy / eV')
+	plt.ylabel('% Error')
+	plt.grid()
+	plt.show()
+
+	plt.figure()
+	plt.hist(percentageError, bins=50)
+	plt.ylabel('Frequency')
+	plt.xlabel('% Error')
+	plt.grid()
+	plt.show()
+
+	countoverthreshold = 0
+	for XX in percentageError:
+		if abs(XX) >= 0.1:
+			countoverthreshold += 1
+
+	percentageOverThreshold = (countoverthreshold / (len(percentageError))) * 100
+
+	print(f'Max error: {np.max(abs(np.array(percentageError)))}')
+	print(f'Mean error: {np.mean(abs(np.array(percentageError)))}')
+	print(f'{percentageOverThreshold} % of points over limit of 0.1 % error')
+
+
+# bounds(minerg, maxerg)
+
+# plt.figure()
+# plt.plot(history.history['loss'])
+# plt.grid()
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.show()
+
+unheated_energies_limited = []
+unheated_XS_limited = []
+for x, h in zip(unheated_energies, unheated_XS):
+	if x <= maxerg and x >= minerg:
+		unheated_energies_limited.append(x)
+		unheated_XS_limited.append(h)
+
+test_energies_limited = []
+predictions_limited = []
+test_XS_limited = []
+for o, p, qx in zip(energies, rescaled_predictions, testxs):
+	if o <= maxerg and o >= minerg:
+		test_energies_limited.append(o)
+		predictions_limited.append(p)
+		test_XS_limited.append(qx)
+
+
+relativeError = []
+percentageError = []
+for p, xs in zip(predictions_limited, test_XS_limited):
+	relativeError.append(abs((p-xs)/xs))
+	percentageError.append((p/xs * 100) - 100)
+
+print('Percentage error:', percentageError)
+# plotdir = '/home/rnt26/PycharmProjects/ResonanceML/AI_broadening/neural_broadening/multinodalplots'
+#
+# plt.figure()
+# plt.plot(test_energies_limited, percentageError, label='Error')
+# plt.xlabel('Energy / eV')
+# plt.ylabel('% Error')
+# # plt.savefig(f'{plotdir}/{timestring}-highres_multinodal_errors.png')
+# plt.grid()
+# plt.show()
+#
+# plt.figure()
+# plt.plot(unheated_energies_limited, unheated_XS_limited, label='0 K JEFF-3.3')
+# plt.grid()
+# plt.plot(test_energies_limited, predictions_limited, label='Predictions', color='red')
+# plt.xlabel('Energy / eV')
+# plt.ylabel('$\sigma_{n,\gamma} / b$')
+# plt.plot(test_energies_limited, test_XS_limited, '--', label=f'{test_temperatures[0]} K JEFF-3.3', color='lightgreen',
+# 		 alpha=0.7)
+# plt.legend()
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.title(f'{periodictable.elements[nuclide[0]]}-{nuclide[1]} $\sigma_{{n,\gamma}}$ at {test_temperatures[0]} K')
+# # plt.savefig(f'{plotdir}/{timestring}-highres_multinodal_plot.png')
+# plt.show()
+
+bounds(minerg, maxerg)
