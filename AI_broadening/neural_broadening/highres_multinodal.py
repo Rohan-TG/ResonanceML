7943c2d693c00d6c27a5e0f09c74fd21d12962f2
Rohan-TG <rohan.tg05@gmail.com>
2025-05-22 20:57:01 +0200

    imports

--- AI_broadening/neural_broadening/highres_multinodal.py
+++ AI_broadening/neural_broadening/highres_multinodal.py
@@ -1,1 +1,19 @@
 import os
+
+os.environ["OMP_NUM_THREADS"] = "30"
+os.environ["MKL_NUM_THREADS"] = "30"
+os.environ["OPENBLAS_NUM_THREADS"] = "30"
+os.environ["TF_NUM_INTEROP_THREADS"] = "30"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "30"
+
+import pandas as pd
+import keras
+import datetime
+import numpy as np
+import random
+import periodictable
+import itertools
+import scipy
+import matplotlib.pyplot as plt
+import tqdm
+from sklearn.metrics import mean_absolute_error
