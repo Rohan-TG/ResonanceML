5be505d107a847efb93f877e417bf10d186cb5b5
Rohan-TG <rohan.tg05@gmail.com>
2025-05-22 23:14:31 +0200

    attempting different model structure

--- AI_broadening/neural_broadening/highres_multinodal.py
+++ AI_broadening/neural_broadening/highres_multinodal.py
@@ -170,12 +170,26 @@ callback = keras.callbacks.EarlyStopping(monitor='val_loss',
 										 start_from_epoch=5,
 										 restore_best_weights=True)
 
+# model = keras.Sequential()
+# model.add(keras.layers.Dense(y_test.shape[1], input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+# model.add(keras.layers.LeakyReLU(alpha=0.2))
+# model.add(keras.layers.Dense(y_test.shape[1]))
+# model.add(keras.layers.LeakyReLU(alpha=0.2))
+# model.add(keras.layers.Dense(y_test.shape[1]))
+# model.add(keras.layers.LeakyReLU(alpha=0.2))
+# # model.add(keras.layers.Dense(y_test.shape[1]))
+# # model.add(keras.layers.LeakyReLU(alpha=0.2))
+# model.add(keras.layers.Dense(y_test.shape[1], activation='linear'))
+# model.compile(loss='mean_absolute_error', optimizer='adam')
+
 model = keras.Sequential()
-model.add(keras.layers.Dense(y_test.shape[1], input_shape=(X_train.shape[1],), kernel_initializer='normal'))
+model.add(keras.layers.Dense(1500, input_shape=(X_train.shape[1],), kernel_initializer='normal'))
 model.add(keras.layers.LeakyReLU(alpha=0.2))
-model.add(keras.layers.Dense(y_test.shape[1]))
+model.add(keras.layers.Dense(1200))
 model.add(keras.layers.LeakyReLU(alpha=0.2))
-model.add(keras.layers.Dense(y_test.shape[1]))
+model.add(keras.layers.Dense(900))
+model.add(keras.layers.LeakyReLU(alpha=0.2))
+model.add(keras.layers.Dense(700))
 model.add(keras.layers.LeakyReLU(alpha=0.2))
 # model.add(keras.layers.Dense(y_test.shape[1]))
 # model.add(keras.layers.LeakyReLU(alpha=0.2))
@@ -183,14 +197,12 @@ model.add(keras.layers.Dense(y_test.shape[1], activation='linear'))
 model.compile(loss='mean_absolute_error', optimizer='adam')
 
 
-
-
 # new additions
 
 history = model.fit(X_train,
 					y_train,
 					epochs=200,
-					batch_size=16,
+					batch_size=32,
 					callbacks=callback,
 					validation_data=(X_val, y_val),
 					verbose=1)
