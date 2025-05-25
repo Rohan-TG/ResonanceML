b27a7b70b7defd133923d1dc1275c6bc12f8ba3b
Rohan-TG <rohan.tg05@gmail.com>
2025-05-25 13:21:33 +0100

    wip

---
+++ AI_broadening/AI_data/Automatic_NJOY.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import subprocess
+
+# temps = np.arange(300.0,1800.1,0.1)
+# Temperatures = []
+# for i in temps:
+# 	Temperatures.append(round(i, 1))
+
+
+Temperatures = [300.0, 2000.0]
+
+deckTemplate = """moder
+20 -21
+reconr
+-21 -22
+'pendf Fe056 JENDL-5 '/
+2631/
+.001/
+0/
+moder
+-22 36
+broadr
+-21 -22 -23
+2631 1/
+.001/
+{broadTemp}/
+0/
+unresr
+-21 -23 -24
+2631 1 1 0/
+{broadTemp}/
+1.e+10/
+0/
+moder
+-24 32
+stop"""
+
+
+for T in Temperatures:
+	deck = deckTemplate.format(broadTemp = T)
+	deckName = f"deck_{T}_K.njoy"
+	with open(f"deck_{T}_K.njoy", "w") as f:
+		f.write(deck)
+
+	t_output = subprocess.Popen(f"ln -sf tape20 fort.20 && /home/rnt26/NJOY/NJOY21/bin/njoy21 < {deckName} && ln -s tape23 fort.23 && /home/rnt26/NJOY/NJOY21/bin/njoy21 < convert_to_ascii.njoy")
+
