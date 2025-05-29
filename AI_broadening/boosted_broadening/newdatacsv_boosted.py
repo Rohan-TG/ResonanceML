65867d315d5dbc10cfed0444ca018f474ad7ba17
Rohan-TG <rohan.tg05@gmail.com>
2025-05-29 22:13:19 +0100

    cpu

--- AI_broadening/boosted_broadening/newdatacsv_boosted.py
+++ AI_broadening/boosted_broadening/newdatacsv_boosted.py
@@ -1,10 +1,10 @@
 import os
 
-os.environ["OMP_NUM_THREADS"] = "60"
-os.environ["MKL_NUM_THREADS"] = "60"
-os.environ["OPENBLAS_NUM_THREADS"] = "60"
-os.environ["TF_NUM_INTEROP_THREADS"] = "60"
-os.environ["TF_NUM_INTRAOP_THREADS"] = "60"
+os.environ["OMP_NUM_THREADS"] = "120"
+os.environ["MKL_NUM_THREADS"] = "120"
+os.environ["OPENBLAS_NUM_THREADS"] = "120"
+os.environ["TF_NUM_INTEROP_THREADS"] = "120"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "120"
 
 
 import random
