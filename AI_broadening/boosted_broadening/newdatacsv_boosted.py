3ec725dd66d9d450664f0130939fdc5fdfc29fb0
Rohan-TG <rohan.tg05@gmail.com>
2025-06-03 22:19:33 +0100

    trying more e

--- AI_broadening/boosted_broadening/newdatacsv_boosted.py
+++ AI_broadening/boosted_broadening/newdatacsv_boosted.py
@@ -1,10 +1,10 @@
 import os
 
-os.environ["OMP_NUM_THREADS"] = "60"
-os.environ["MKL_NUM_THREADS"] = "60"
-os.environ["OPENBLAS_NUM_THREADS"] = "60"
-os.environ["TF_NUM_INTEROP_THREADS"] = "60"
-os.environ["TF_NUM_INTRAOP_THREADS"] = "60"
+os.environ["OMP_NUM_THREADS"] = "90"
+os.environ["MKL_NUM_THREADS"] = "90"
+os.environ["OPENBLAS_NUM_THREADS"] = "90"
+os.environ["TF_NUM_INTEROP_THREADS"] = "90"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "90"
 
 
 import random
